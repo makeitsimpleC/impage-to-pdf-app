0e9c21997cd89fe2317302f59f4da9295ef30465
makeitsimpleC <convert@makeit-simple.de>
2025-04-11 04:39:08 +0200

    Update app.py

--- image-to-pdf-app/app.py
+++ image-to-pdf-app/app.py
@@ -28,4 +28,5 @@ def convert():
         return {'error': str(e)}, 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
